50e7ca0663ddb5b1f0e2305c6994135255225996
SecretIGO <110442934+SecretIGO@users.noreply.github.com>
2023-07-10 23:24:43 +0800

    flask and react connection solidified

--- flaskApplication/model_connector.py
+++ flaskApplication/model_connector.py
@@ -1,14 +1,19 @@
-import mysql.connector
+import pymysql
 
 import dbcon_items
 import dbcon_store
 import dbcon_user
 
-mydb = mysql.connector.connect(
-  host="localhost",
+mydb = pymysql.connect(
+  host="127.0.0.1",
   user="root",
   password="root",
   database="db_onphar"
 )
 
 mycursor = mydb.cursor()
+
+result = dbcon_user.find_username("", mycursor)
+
+print(result)
+print(dbcon_user.get_userInformation("johndoe", mycursor))
