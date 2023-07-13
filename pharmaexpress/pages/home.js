2f3208629b60343302eef4a8f4ef3a86ff4f2a68
Justine <justinebenedictumali@gmail.com>
2023-07-13 23:52:48 +0800

    Update featured product component

--- pharmaexpress/pages/home.js
+++ pharmaexpress/pages/home.js
@@ -3,11 +3,12 @@ import Header from '../components/navbar.js'
 import NewProducts from '../components/newproducts.js'
 import Data from '../json/Products.json'
 
+
 export default function Home({featuredProduct, newProducts}){
     return(
         <div>
             <Header/>
-            <Featured/>
+            <Featured product={featuredProduct}/>
             <NewProducts products={newProducts}/>
         </div>
     )
@@ -15,7 +16,7 @@ export default function Home({featuredProduct, newProducts}){
 
 export async function getStaticProps() {
 
-    const featuredProductId = 'your-featured-product-id';
+    const featuredProductId = Math.floor(Math.random() * Object.keys(Data).length)
     const featuredProduct = Data.find((product) => product.id === featuredProductId);
   
     const newProducts = Data.slice(0, 10);
@@ -24,6 +25,7 @@ export async function getStaticProps() {
     return {
       props: {
         newProducts,
+        featuredProduct,
       },
     };
   }
