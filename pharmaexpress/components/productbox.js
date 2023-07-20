c955664578f4d32718ad2385eef5cc0acc823bdf
SecretIGO <110442934+SecretIGO@users.noreply.github.com>
2023-07-20 09:54:12 +0800

    commit for product box

--- pharmaexpress/components/productbox.js
+++ pharmaexpress/components/productbox.js
@@ -1,43 +1,32 @@
+import Link from 'next/link'
 import styles from '../styles/ProductBox.module.css'
+import Image from 'next/image';
 
-export default function ProductBox({product,id}){
+export default function ProductBox({product}){
 
-    const url = `/product/${id}`
-
-    //const cartItems = useContext(cartContext)
-
-    const addProduct = (product) =>{
-        const newProduct = {
-            ...product,
-            count: 1,
-        }
-        setProducts([
-            ...cartItems,
-            newProduct,
-        ])
-    }
+    const url = `/product/${product.id}`
 
     return(
         <div>
-            <div className={styles.white_box}>
+            <Link href={url}>
+                <div className={styles.white_box}>
                     <img className={styles.imgStyle} src={product.image}/>
-            </div>
-            <div className={styles.product_info}>
-                <h1 className='text-lg font-bold p-2'>
-                    {product.drug_brand}
-                </h1>
-                
-                <div className={styles.price_row}>
-                    <div className='text-xl font-light text-right'>
-                        {product.price}
-                    </div>
-                    <div className='bg-blue-700 text-center text-sm p-3 rounded-full font-bold text-white border shadow-md'>
-                        <button onClick={() => addProduct(product)}>
-                            Add to cart
-                        </button>
-                    </div>
-                </div>     
-            </div>
+                </div>
+                <div className={styles.product_info}>
+                    <h1 className='text-lg font-bold p-2'>
+                        {product.name}
+                    </h1>
+                    
+                    <div className={styles.price_row}>
+                        <div className='text-xl font-light text-right'>
+                            P {product.price}
+                        </div>
+                        <div className='bg-blue-700 text-center text-sm p-3 rounded-full font-bold text-white border shadow-md'>
+                                Read More
+                        </div>
+                    </div>     
+                </div>
+            </Link>
         </div>
     )
 }
